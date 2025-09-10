0878bc04de092192e8dde0956b1d2a9198c7c4bc
Geordan Gesink <georgegeordan@gmail.com>
2025-09-10 19:30:49 +0200

    lint

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const { isBareKit, isPear, isIOS, isAndroid } = require('which-runtime')
 
 // port to which-runtime
-const isReactNative = typeof global !== 'undefined' && 
-  (global.navigator?.product === 'ReactNative' || 
-   global.__DEV__ === true || 
+const isReactNative = typeof global !== 'undefined' &&
+  (global.navigator?.product === 'ReactNative' ||
+   global.__DEV__ === true ||
    typeof global.HermesInternal !== 'undefined' ||
    (typeof global.require !== 'undefined' && global.require.main?.filename?.includes('react-native')))
 
-const isExpo = typeof global !== 'undefined' && 
-  (global.__expo || 
-   global.ExpoModules || 
+const isExpo = typeof global !== 'undefined' &&
+  (global.__expo ||
+   global.ExpoModules ||
    (typeof global.require !== 'undefined' && global.require.main?.filename?.includes('expo')))
 
 const isMobile = isReactNative || isExpo || isIOS || isAndroid
 
 if (isPear) {
-  module.exports= eval('require')('./lib/pear.js')
-}
-else if (isBareKit || isMobile) {
-  module.exports= require('./lib/bare-kit.js')
+  module.exports = eval('require')('./lib/pear.js') // eslint-disable-line no-eval
+} else if (isBareKit || isMobile) {
+  module.exports = require('./lib/bare-kit.js')
 }
