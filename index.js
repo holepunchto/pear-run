1abf1fb51ee46e0a9438c653f2dfb499509b5a3a
Geordan Gesink <georgegeordan@gmail.com>
2025-09-11 23:14:10 +0200

    move files to root

--- index.js
+++ index.js
@@ -1,21 +1,56 @@
-const { isBareKit, isPear, isIOS, isAndroid, isBare } = require('which-runtime')
+'use strict'
+/* globals Pear */
+const { isBare } = require('which-runtime')
+const ref = require('pear-ref')
+const plink = require('pear-link')
+const pear = require('pear-cmd')
+const b4a = require('b4a')
+const hypercoreid = require('hypercore-id-encoding')
+const rundef = require('pear-cmd/run')
+const { command } = require('paparam')
+const { spawn } = isBare ? require('bare-subprocess') : require('child_process')
+const program = global.Bare ?? global.process
 
-// port to which-runtime
-const isReactNative = typeof global !== 'undefined' &&
-  (global.navigator?.product === 'ReactNative' ||
-   global.__DEV__ === true ||
-   typeof global.HermesInternal !== 'undefined' ||
-   (typeof global.require !== 'undefined' && global.require.main?.filename?.includes('react-native')))
-
-const isExpo = typeof global !== 'undefined' &&
-  (global.__expo ||
-   global.ExpoModules ||
-   (typeof global.require !== 'undefined' && global.require.main?.filename?.includes('expo')))
-
-const isMobile = isReactNative || isExpo || isIOS || isAndroid
-
-if (isBareKit || isMobile) {
-  module.exports = require('./lib/bare-kit.js')
-} else if (isPear || isBare) {
-  module.exports = eval('require')('./lib/pear.js') // eslint-disable-line no-eval
+module.exports = function run (link, args = []) {
+  const { RUNTIME, RUNTIME_ARGV, RTI } = Pear.constructor
+  let parsed = null
+  try {
+    parsed = plink.parse(link)
+  } catch (err) {
+    if (err.info?.hostname === 'dev') return run('.' + err.info.pathname)
+    throw err
+  }
+  const { key, fork, length } = parsed.drive
+  const { key: appKey } = Pear.app.applink ? (plink.parse(Pear.app.applink)).drive : {}
+  if (appKey && key && b4a.equals(key, appKey) && fork === null && length === null) {
+    link = `pear://${Pear.app.fork}.${Pear.app.length}.${hypercoreid.encode(key)}${parsed.pathname || ''}`
+  }
+  const argv = pear(program.argv.slice(1)).rest
+  const parser = command('run', ...rundef)
+  const cmd = parser.parse(argv, { sync: true })
+  const inject = [link]
+  if (!cmd.flags.trusted) inject.unshift('--trusted')
+  if (RTI.startId) inject.unshift('--parent', RTI.startId)
+  argv.length = cmd.indices.args.link
+  argv.push(...inject)
+  argv.unshift('run')
+  let linksIndex = cmd.indices.flags.links
+  const linksElements = linksIndex > 0 ? (cmd.flags.links === argv[linksIndex]) ? 2 : 1 : 0
+  if (cmd.indices.flags.startId > 0) { // todo: dead code?
+    argv.splice(cmd.indices.flags.startId, 1)
+    if (linksIndex > cmd.indices.flags.startId) linksIndex -= linksElements
+  }
+  if (linksIndex > 0) argv.splice(linksIndex, linksElements)
+  const sp = spawn(RUNTIME, [...RUNTIME_ARGV, ...argv, ...args], {
+    stdio: ['inherit', 'inherit', 'inherit', 'overlapped'],
+    windowsHide: true
+  })
+  ref.ref()
+  sp.once('exit', (exitCode) => {
+    if (exitCode !== 0) pipe.emit('crash', { exitCode })
+    ref.unref()
+  })
+  const pipe = sp.stdio[3]
+  pipe.on('end', () => pipe.end())
+  return pipe
 }
