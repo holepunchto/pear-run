98c70bdd9eabed9d085db934eb772b05f827e30f
Geordan Gesink <georgegeordan@gmail.com>
2025-09-12 00:13:40 +0200

    fix exports

---
+++ index.mobile.js
@@ -0,0 +1,8 @@
+module.exports = async function run(filename, source, args = []) {
+  const { Worklet } = require('react-native-bare-kit')
+
+  const worklet = new Worklet()
+  await worklet.start(filename, source, args)
+
+  return worklet
+}
