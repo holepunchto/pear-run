e81e5da31050b0a35f1a4bff29e20351b68b942b
Geordan Gesink <georgegeordan@gmail.com>
2025-10-30 03:17:12 +0100

    fix

--- index.mobile.js
+++ index.mobile.js
@@ -1,9 +1,1 @@
-const { isIOS, isAndroid, isExpo, isReactNative } = require('which-runtime')
-const isMobile = isIOS || isAndroid || isExpo || isReactNative
-
-try{
-if (isMobile && !isReactNative) module.exports = require('./lib/mobile.js')
-else module.exports = require('./lib/react-native.js')
-} catch (err){
-  console.error(err)
-}
+module.exports = require('./lib/react-native.js')
