d18fb72acbeb816d9717391738e26cd19ee856b8
Geordan Gesink <georgegeordan@gmail.com>
2025-11-20 04:42:53 +0100

    lint

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const { isPear } = require('which-runtime')
 
-try{
-if (!isPear) module.exports = require('./lib/mobile.js') // TODO: need different check when we add Pear global to mobile
-else module.exports = require('./lib/desktop.js')
-} catch (err){
+try {
+  if (!isPear)
+    module.exports = require('./lib/mobile.js') // TODO: need different check when we add Pear global to mobile
+  else module.exports = require('./lib/desktop.js')
+} catch (err) {
   console.error(err)
 }
