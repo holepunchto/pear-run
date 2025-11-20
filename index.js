9ccf475dca4bb1eb633a7452997d23306b30f6c6
Geordan Gesink <georgegeordan@gmail.com>
2025-11-20 04:39:52 +0100

    add comment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { isPear } = require('which-runtime')
 
 try{
-if (!isPear) module.exports = require('./lib/mobile.js')
+if (!isPear) module.exports = require('./lib/mobile.js') // TODO: need different check when we add Pear global to mobile
 else module.exports = require('./lib/desktop.js')
 } catch (err){
   console.error(err)
