fd462fd709278bd2d655272b860b46d9605b2236
Geordan Gesink <georgegeordan@gmail.com>
2026-01-19 21:40:48 +0100

    change link map path

--- index.rn.js
+++ index.rn.js
@@ -1,6 +1,6 @@
 module.exports = function run(link = 'main', args = []) {
   const { Worklet } = require('react-native-bare-kit')
-  const map = require('../../.pear/bundles/pear-links-map-rn')
+  const map = require('../../.pear/utils/pear-links-map-rn')
   const bundle = map[link]?.bundle
   if ( !bundle ) throw new Error(`could not find bundle for ${link}`)
   if ( link.startsWith('pear://') ) link = map[link]?.hash
