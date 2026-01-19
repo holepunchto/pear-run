1c67c8d41eb188a093e6dc7311262904ee4eab31
Geordan Gesink <georgegeordan@gmail.com>
2026-01-19 19:34:36 +0100

    move bundle maps to .pear folder

--- index.mobile.js
+++ index.mobile.js
@@ -1,5 +1,5 @@
 const Worker = require('bare-worker')
-const linkmapper = require('pear-mobile/map')
+const linkmapper = require('pear-links-map') // will be transformed in bundler to require the actual file
 const b4a = require('b4a')
 
 module.exports = (link, args= [], data = {}) => {
@@ -8,7 +8,7 @@ module.exports = (link, args= [], data = {}) => {
     const key = link
     link = linkmapper[link]?.resolved
     if (!link)
-      throw new Error(`Could not find bundle for '${key}' in pear-mobile/map`)
+      throw new Error(`Could not find bundle for '${key}' in pear-links-map`)
   }
   Worker.preload(require.resolve('pear-api'))
   const worker = new Worker(link, {
