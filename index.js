9d77eecad1e1a6b9b7e4cbe473197abbc0b686df
Geordan Gesink <georgegeordan@gmail.com>
2026-02-10 00:54:06 +0100

    unsupport pear links; add worker config links

--- index.js
+++ index.js
@@ -1,53 +1,38 @@
 'use strict'
 /* globals Pear */
 const ref = require('pear-ref')
-const plink = require('pear-link')
 const pear = require('pear-cmd')
-const b4a = require('b4a')
 const rundef = require('pear-cmd/run')
 const { command } = require('paparam')
 const { spawn } = require('child_process')
-const { pathToFileURL } = require('url-file-url')
+const { pathToFileURL, fileURLToPath } = require('url-file-url')
 const path = require('path')
-const { ERR_NOT_FOUND } = require('pear-errors')
+const { ERR_NOT_FOUND, ERR_INVALID_INPUT } = require('pear-errors')
 const { isElectronRenderer } = require('which-runtime')
 const unixpathresolve = require('unix-path-resolve')
 const program = global.Bare ?? global.process
 
+let check
 module.exports = function run(link, args = []) {
-  const isPear = link.startsWith('pear://')
   const isFile = link.startsWith('file://')
-  const isPath = isPear === false && isFile === false
-  const isAbsolute = isPath && path.isAbsolute(link)
-
+  const isPear = link.startsWith('pear://')
+  const isAbsolute = !isFile && path.isAbsolute(link)
+  
   const app = Pear.app ?? Pear.config // note: legacy, remove in future
+  if (!isFile && !isAbsolute && !isPear) {
+    if (app.options.workers === undefined) throw new Error('workers undefined')
+    const worker = app.options.workers[link]
+    if (typeof worker !== 'string') throw new Error(`worker "${link}" not found`)
+    const workerLink = app.applink.startsWith('pear://') ?
+      app.applink + worker :
+      pathToFileURL(path.join(fileURLToPath(app.applink), worker.slice(1))).href.replaceAll('%23', '#')
+    check = 1
+    return run(workerLink, args)
+  }
+  if (isPear && check !== 1) throw ERR_INVALID_INPUT('pear links not supported')
 
   const { RUNTIME, RUNTIME_ARGV, RTI, RUNTIME_FLAGS = [] } = Pear.constructor
-  let parsed = null
-  try {
-    parsed = plink.parse(link)
-  } catch (err) {
-    if (err.info?.hostname === 'dev')
-      return run(path.resolve('.' + err.info.pathname))
-    throw err
-  }
-  const { key, fork, length } = parsed.drive
-
-  const applink = plink.parse(app.applink)
-  const { key: appKey } = applink.drive
-  if (
-    appKey &&
-    key &&
-    b4a.equals(key, appKey) &&
-    fork === null &&
-    length === null
-  ) {
-    link = plink.serialize({
-      ...parsed,
-      drive: { ...parsed.drive, length: app.length, fork: app.fork }
-    })
-  }
-
+    
   if (isElectronRenderer) {
     if (typeof Pear[Pear.constructor.IPC]?.run === 'function') {
       return Pear[Pear.constructor.IPC].run(link, args)
@@ -56,10 +41,10 @@ module.exports = function run(link, args = []) {
     }
   }
 
-  if (isPath) {
+  if (!isFile) {
     unixpathresolve('/', link) // throw if escaping root
     if (isAbsolute) link = pathToFileURL(link).href.replaceAll('%23', '#')
-    else ERR_NOT_FOUND('not found (path must be absolute)')
+    else if (!isPear) throw ERR_NOT_FOUND('not found (path must be absolute)')
   }
 
   const argv = pear(program.argv.slice(1)).rest
@@ -70,7 +55,7 @@ module.exports = function run(link, args = []) {
   if (RTI.startId) inject.unshift('--parent', RTI.startId)
   if (
     app.key === null &&
-    isPath &&
+    !isFile &&
     (!isAbsolute || link.startsWith(app.applink + '/'))
   ) {
     inject.unshift('--base', app.dir)
